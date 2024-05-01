15640c824c962dade94e5f139b0baba83992fb53
thtskaran <hello@karanprasad.com>
2024-05-01 14:16:38 +0000

    Implemented multiprocessing in FrameEnhancer

--- FrameEnhancer.py
+++ FrameEnhancer.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import os
+import multiprocessing
 
 from frame_optimization_methods.opticalFlow import remove_dead_frames as remove_dead_frames_of
 from frame_optimization_methods.frameDifference import remove_dead_frames as remove_dead_frames_fd
@@ -51,21 +52,32 @@ def main():
   base_threshold = 10  # Default threshold for frame difference
   flow_mag_threshold = 0.4  # Default threshold for optical flow
 
+  processes = []
+
   if args.opticalFlow:
     print("Using Optical Flow method...")
-    remove_dead_frames_of(args.video, flow_mag_threshold)
+    p1 = multiprocessing.Process(target=remove_dead_frames_of, args=(args.video, flow_mag_threshold))
+    processes.append(p1)
+    p1.start()
 
-  elif args.frameDifference:
+  if args.frameDifference:
     print("Using Frame Difference method...")
-    remove_dead_frames_fd(args.video, base_threshold)
+    p2 = multiprocessing.Process(target=remove_dead_frames_fd, args=(args.video, base_threshold))
+    processes.append(p2)
+    p2.start()
 
-  elif args.ssim:
+  if args.ssim:
     print("Using SSIM method...")
-    output_video = os.path.splitext(
-        args.video)[0] + "_ssim.mp4"  # Generating output file name
-    process_video_ssim(args.video, args.ssim_threshold, output_video)
+    output_video = os.path.splitext(args.video)[0] + "_ssim.mp4"  # Generating output file name
+    p3 = multiprocessing.Process(target=process_video_ssim, args=(args.video, args.ssim_threshold, output_video))
+    processes.append(p3)
+    p3.start()
 
-  else:
+  # Wait for all processes to finish
+  for process in processes:
+    process.join()
+
+  if not processes:
     print(
         "Error: No method selected. Use -of for Optical Flow, -fd for Frame Difference, or -ss for SSIM."
     )
